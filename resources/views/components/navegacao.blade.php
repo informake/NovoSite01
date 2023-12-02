6e475842d4acb918d8e6490a8c466eef4ee3c812
informake <gildvan.arley@gmail.com>
2023-12-02 13:54:25 -0300

    tema

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,36 @@
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="sidebarMenuLabel">Gestão</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+              <svg class="bi"><use xlink:href="#house-fill"/></svg>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+              Vendas
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#cart"/></svg>
+              Produto
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#people"/></svg>
+              Clientes
+            </a>
+          </li>
+        </ul>        
+      </div>
+    </div>
+  </div>
